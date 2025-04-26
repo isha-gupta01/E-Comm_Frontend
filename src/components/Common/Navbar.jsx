dcc96a36a6bfc8ba3b7e7eaf101c598cbb153ee5
Bloomer004 <ishaguptavnsv@gmail.com>
2025-04-26 18:46:58 +0530

    Logo

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
                     <button onClick={toggleCartDrawer} className='relative hover:text-black'>
                         <HiOutlineShoppingBag className='h-6 w-6 text-gray-700' />
                         {cartItemCount > 0 && (
-                            <span className='absolute -top-1 bg-green-50 text-white text-xs rounded-full px-2 py-0.5'>
+                            <span className='absolute -top-1 bg-emerald-600 text-white text-xs rounded-full px-2 py-0.5'>
                                 {cartItemCount}
                             </span>
                         )}
